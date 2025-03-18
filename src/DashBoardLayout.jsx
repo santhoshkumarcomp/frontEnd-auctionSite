52cb567f2f6c3285ec68ba02f51c744e9b56d62c
santhoshkumarcomp <santhoshkumarcomp@gmail.com>
2025-03-18 10:38:44 +0530

    changed

--- src/DashBoardLayout.jsx
+++ src/DashBoardLayout.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router"
+import { Link, Outlet, useNavigate } from "react-router"
 import "./DashBoardLayout.css"
 import importImg from './5911.jpg';
 import Me from "./Me";
@@ -10,10 +10,12 @@ import axios from "axios";
 
 const DashBoardLayout = () => {
   const {user,logged} = useContext(AuthContext);
+  const navigate = useNavigate();
   const handleRefresh = async(e) => {
     e.preventDefault;
     const response = await axios.get('https://be-capstone-5rvf.onrender.com/seller/post/closedposts', { withCredentials: true });
     console.log(response.data);
+    navigate("/seller/posts");
     alert("Refreshed");
 
   }
@@ -23,10 +25,11 @@ const DashBoardLayout = () => {
     {logged && <div className=" sticky rounded-xl min-h-screen flex-[0.2] flex flex-col border border-gray-500 bg-white drop-shadow-lg text-[21px] bg-opacity-25">
       <div className="  hover:cursor-pointer hover:bg-opacity-50"><Link to={`/${user}/posts`}><img src={importImg} className="w-[90px] h-[80px] inline" ></img><h1 className="inline align-center   julius-sans-one-regular font-extrabold">AUCTION's</h1></Link></div>
       <div className=" border rounded-sm border-dashed border-orange-500 p-[10px] m-[10px] flex align-center items-center hover:cursor-pointer hover:bg-yellow-100"><i className="fa-solid fa-house"></i><span className="relative left-1/5 text-gray-500"><Link to={`/${user}/posts`}>Home</Link></span></div>
-      {user==="seller" ?(
-      <div className=" border rounded-sm border-dashed border-orange-500 p-[10px] m-[10px] flex align-center items-center hover:cursor-pointer hover:bg-yellow-100"><i className="fa-solid fa-square-plus"></i><span className="relative left-1/5 text-gray-500"><Link to={"/seller/createpost"}>Create Post</Link></span></div>):
+      {user==="seller" ?(<>
+      <div className=" border rounded-sm border-dashed border-orange-500 p-[10px] m-[10px] flex align-center items-center hover:cursor-pointer hover:bg-yellow-100"><i className="fa-solid fa-square-plus"></i><span className="relative left-1/5 text-gray-500"><Link to={"/seller/createpost"}>Create Post</Link></span></div>
+      <div  onClick={handleRefresh} className=" border rounded-sm border-dashed border-orange-500 p-[10px] m-[10px] flex align-center items-center hover:cursor-pointer hover:bg-yellow-100"><i className="fa-solid fa-dharmachakra"></i><span className="relative left-1/5 text-gray-500">Refresh</span></div></>):
       <div className=" border rounded-sm border-dashed border-orange-500 p-[10px] m-[10px] flex align-center items-center hover:cursor-pointer hover:bg-yellow-100"><i className="fa-solid fa-square-plus"></i><span className="relative left-1/5 text-gray-500"><Link to={"/seller/createpost"}>History</Link></span></div>}
-      <div  onClick={handleRefresh} className=" border rounded-sm border-dashed border-orange-500 p-[10px] m-[10px] flex align-center items-center hover:cursor-pointer hover:bg-yellow-100"><i className="fa-solid fa-dharmachakra"></i><span className="relative left-1/5 text-gray-500">Refresh</span></div>
+      
       {/* <div className=" border rounded-sm border-dashed border-orange-500 p-[10px] m-[10px] flex align-center items-center hover:cursor-pointer hover:bg-yellow-100"><i className="fa-solid fa-rotate-left"></i><span className="relative left-1/5 text-gray-500">Reverse</span></div>
       <div className=" border rounded-sm border-dashed border-orange-500 p-[10px] m-[10px] flex align-center items-center hover:cursor-pointer hover:bg-yellow-100"><i className="fa-solid fa-envelope-circle-check"></i><span className="relative left-1/5 text-gray-500">Sealed</span></div>     */}
       
