491908e409b2accc9db6cda7913fe3ad9b8e2839
santhoshkumarcomp <santhoshkumarcomp@gmail.com>
2025-04-01 10:27:44 +0530

    added alert

--- src/UserRegistrationForm.jsx
+++ src/UserRegistrationForm.jsx
@@ -47,7 +47,7 @@ const UserRegistrationForm = () => {
           password: formData.password,
         })
         .then(function (response) {
-          alert(response);
+          alert(response.data);
         })
         .catch(function (error) {
           console.log(error);
